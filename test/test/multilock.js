1d919cbd9eea7741aa6f4ca842565b8bc4e29612
Eric Vallee <eric_vallee@webificservices.com>
2016-07-01 17:52:46 -0400

    More tests and bug fix

--- test/test/multilock.js
+++ test/test/multilock.js
@@ -23,7 +23,7 @@ exports.main = {
         });
     },
     'cooperative_locks': function(test) {
-        test.expect(0);
+        test.expect(3);
         var aLock = null;
         var bLock = null;
         var testCol = db.collection('test');
@@ -42,11 +42,27 @@ exports.main = {
                 timeout: 1000,
                 w: 1
             });
-            multiLock.acquire({'_id': 1}, {tag: 'a'}).then((lock) => {
+            return multiLock.acquire({'_id': 1}, {'tag': 'a'}).then((lock) => {
                 aLock = lock;
             }).then(() => {
-                return multiLock.acquire({'_id': 1}, {tag: 'b'}).catch((err) => {
-                    console.log(err);
+                return multiLock.acquire({'_id': 1}, {'tag': 'b'}).catch((err) => {
+                    test.ok(err && err.output && err.output.payload && err.output.payload.statusCode == 409 && err.output.payload.message == 'CooperativeLock', "Ensuring cooperative locks prevent access.");
+                });
+            }).then(() => {
+                return multiLock.release({'_id': 1}, {'lock': aLock, 'tag': 'a'}).then(() => {
+                    return multiLock.acquire({'_id': 1}, {'tag': 'a'});
+                }).then((lock) => {
+                    aLock = lock;
+                    test.ok(lock, "Ensure that acquiring a lock failed without holding the lock.");
+                });
+            }).then(() => {
+                return multiLock.release({'_id': 1}, {'lock': aLock, 'tag': 'a'}).then(() => {
+                    return multiLock.acquire({'_id': 1}, {'tag': 'b'});
+                }).then((lock) => {
+                    bLock = lock;
+                    test.ok(lock, "Ensure that releasing a lock works.");
+                }).then(() => {
+                    return multiLock.release({'_id': 1}, {'lock': bLock, 'tag': 'b'});
                 });
             });
         }).catch((err) => {
@@ -57,6 +73,10 @@ exports.main = {
         
     },
     'aggressive_locks': function(test) {
+        test.expect(0);
+        test.done();
+    },
+    'concurrent_locks': function(test) {
         test.expect(0);
         test.done();
     },
